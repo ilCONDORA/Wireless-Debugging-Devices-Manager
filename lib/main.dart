c16b3aebcf40df041872e70c08af7753c31fbf35
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2024-08-30 12:45:53 +0200

    implemented minimum window size
    implemented minimum window size using the package 'window_manager'. It works on Windows and it needs to be tested on MacOS and Linux.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:wireless_debugging_devices_manager/bloc/theme_bloc.dart';
 import 'package:wireless_debugging_devices_manager/config/app_theme.dart';
-import 'package:wireless_debugging_devices_manager/widgets/condor_switch_theme_mode.dart';
+import 'package:wireless_debugging_devices_manager/screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,9 +15,21 @@ Future<void> main() async {
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorage
-            .webStorageDirectory // Web storage whic is not used for this software
+            .webStorageDirectory // Web storage which is not used for this software
         : await getApplicationSupportDirectory(), // Store data in the \AppData\Roaming\ilCONDORA folder in Windows and /.local/share in Linux, idk for MacOS.
   );
+
+  /// Set the minimum size of the window
+  await windowManager.ensureInitialized();
+  WindowOptions windowOptions = const WindowOptions(
+    minimumSize: Size(999, 777),
+    center: true,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MainApp());
 }
 
@@ -38,9 +51,7 @@ class MainApp extends StatelessWidget {
             theme: CondorAppTheme.lightTheme,
             darkTheme: CondorAppTheme.darkTheme,
             themeMode: state.themeMode,
-            home: const Scaffold(
-              body: CondorSwitchThemeMode(),
-            ),
+            home: const HomeScreen(),
           );
         },
       ),
