25060606bd4c7b1347a4611b6e8528c77e0f699e
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2024-08-30 15:09:08 +0200

    Create condor_buttons_row.dart

---
+++ lib/widgets/condor_buttons_row.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:wireless_debugging_devices_manager/widgets/condor_switch_theme_mode.dart';
+
+class CondorButtonsRow extends StatelessWidget {
+  const CondorButtonsRow({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(Colors.lightBlue.shade300),
+          ),
+          child: const Text('Add Device'),
+        ),
+        const SizedBox(
+          width: 33,
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.deepOrange),
+          ),
+          child: const Text('Kill Adb Server'),
+        ),
+        const SizedBox(
+          width: 33,
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.lime),
+          ),
+          child: const Text('Info Page'),
+        ),
+        const SizedBox(
+          width: 33,
+        ),
+        const CondorSwitchThemeMode(),
+      ],
+    );
+  }
+}
