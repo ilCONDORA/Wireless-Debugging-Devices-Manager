a3cc42cdc89741c40b8c9ff6d58cee7b554c1e9e
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2024-09-01 15:08:43 +0200

    renamed software informations
    the package 'rename' is a scam, it renames two things. This package 'package_rename' seems like the real deal.

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "wireless_debugging_devices_manager");
+    gtk_header_bar_set_title(header_bar, "Wireless Debugging Devices Manager");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "wireless_debugging_devices_manager");
+    gtk_window_set_title(window, "Wireless Debugging Devices Manager");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
