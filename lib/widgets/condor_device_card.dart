6b4d7b9a3f0c6a89837e071e93ca1cf43e97b578
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2024-09-06 15:45:07 +0200

    Revamped condor_device_card
    instead of SelectableText its a read only TextField. Added CircularProgressIndicator. Next phase is to add the data of device infos, visibility of CircularProgressIndicator and disabled state of the buttons

--- lib/widgets/condor_device_card.dart
+++ lib/widgets/condor_device_card.dart
@@ -9,90 +9,190 @@ class CondorDeviceCard extends StatelessWidget {
       width: 555,
       child: Card(
         color: Colors.grey.shade400,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
+        child: const Padding(
+          padding: EdgeInsets.symmetric(
             vertical: 12,
-            horizontal: 16,
+            horizontal: 11,
           ),
           child: Row(
             children: [
-              SizedBox(
-                width: 300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //TODO: use input with disabled without it's theme because it's prettier and Text needs to be always black.
-                    Row(
-                      children: [
-                        const SelectableText(
-                            'Complete IP Address\n192.168.1.1:5555'),
-                        const SizedBox(
-                          width: 14,
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.edit),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const SelectableText(
-                            'Custom name\nSamsung Galaxy S22+'),
-                        const SizedBox(
-                          width: 14,
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.edit),
-                        ),
-                      ],
-                    ),
-                    const SelectableText('Serial number\nXJYPL678QWE'),
-                    const SelectableText('Model\nSM_S906B'),
-                    const SelectableText('Manufacturer\nSamsung'),
-                    const SelectableText('Android version\n14'),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Wrap(
-                  direction: Axis.vertical,
-                  runAlignment: WrapAlignment.end,
-                  crossAxisAlignment: WrapCrossAlignment.end,
-                  spacing: 28,
-                  children: [
-                    //TODO: change theme of onPressed: null to have it opaque and add spinner.
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Connect'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Disconnect'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Run tcpip'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Mirror screen'),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.delete,
-                        color: Colors.red.shade600,
-                        size: 44,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              CondorDeviceInfos(),
+              CondorColumnButtons(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CondorDeviceInfos extends StatelessWidget {
+  const CondorDeviceInfos({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 333,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const Expanded(
+                child: TextField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    labelText: 'Complete IP Address',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.edit),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Row(
+            children: [
+              const Expanded(
+                child: TextField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    labelText: 'Custom name',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.edit),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: 'Serial number',
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: 'Model',
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: 'Manufacturer',
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: 'Android version',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CondorColumnButtons extends StatelessWidget {
+  const CondorColumnButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Wrap(
+        direction: Axis.vertical,
+        runAlignment: WrapAlignment.end,
+        crossAxisAlignment: WrapCrossAlignment.end,
+        spacing: 36,
+        children: [
+          Row(
+            children: [
+              const SizedBox(
+                width: 24,
+                height: 24,
+                child: Visibility(
+                  visible: true,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2.5,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 14,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Connect'),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 24,
+                height: 24,
+                child: Visibility(
+                  visible: false,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2.5,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 14,
+              ),
+              ElevatedButton(
+                onPressed: null,
+                child: Text('Disconnect'),
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Run tcpip'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Mirror screen'),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.delete,
+              color: Colors.red.shade600,
+              size: 44,
+            ),
+          ),
+        ],
       ),
     );
   }
