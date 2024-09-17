2e35a9682dfee6196263f544e6ca26d8a981e78c
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2024-09-17 10:14:25 +0200

    updated AppSettingsModel
    updated AppSettingsModel so that now it has a Locale data

--- lib/models/app_settings_model.dart
+++ lib/models/app_settings_model.dart
@@ -3,18 +3,26 @@ import 'package:flutter/material.dart';
 /// Model for the application settings.
 class AppSettingsModel {
   final ThemeMode themeMode;
+  final Locale locale;
 
   /// Sets the default value for the application theme.
   static const ThemeMode defaultThemeMode = ThemeMode.light;
 
+  /// Sets the default value for the application locale.
+  static const Locale defaultLocale = Locale('en');
+
   /// Constructor for creating an AppSettings instance.
-  AppSettingsModel({this.themeMode = defaultThemeMode});
+  AppSettingsModel({
+    this.themeMode = defaultThemeMode,
+    this.locale = defaultLocale,
+  });
 
   /// Create an AppSettings instance from a JSON map.
   factory AppSettingsModel.fromJson(Map<String, dynamic> json) {
     return AppSettingsModel(
       themeMode:
           ThemeMode.values[json['themeMode'] as int? ?? defaultThemeMode.index],
+      locale: Locale(json['locale'] as String? ?? defaultLocale.languageCode),
     );
   }
 
@@ -22,14 +30,19 @@ class AppSettingsModel {
   Map<String, dynamic> toJson() {
     return {
       'themeMode': themeMode.index,
+      'locale': locale.languageCode,
     };
   }
 
   /// Creates a copy of the current AppSettingsModel with the option to modify specific properties. If a property is not provided, the current value is retained.
   /// This method is used to create a new instance of AppSettingsModel with modified properties.
-  AppSettingsModel copyWith({ThemeMode? themeMode}) {
+  AppSettingsModel copyWith({
+    ThemeMode? themeMode,
+    Locale? locale,
+  }) {
     return AppSettingsModel(
       themeMode: themeMode ?? this.themeMode,
+      locale: locale ?? this.locale,
     );
   }
 }
