458bc0e2da06d628bef277047c7a63a5feda9c1c
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2024-09-17 11:35:27 +0200

    Update condor_device_card.dart
    moved the BlocBuilder

--- lib/widgets/condor_device_card.dart
+++ lib/widgets/condor_device_card.dart
@@ -12,16 +12,27 @@ class CondorDeviceCard extends StatelessWidget {
       width: 555,
       child: Card(
         color: Colors.grey.shade400,
-        child: const Padding(
-          padding: EdgeInsets.symmetric(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
             vertical: 12,
             horizontal: 11,
           ),
-          child: Row(
-            children: [
-              CondorDeviceInfos(),
-              CondorColumnButtons(),
-            ],
+          child: BlocBuilder<AppSettingsBloc, AppSettingsState>(
+            buildWhen: (previous, current) =>
+                previous.appSettingsModel.locale !=
+                current.appSettingsModel.locale,
+            builder: (context, state) {
+              // ignore: prefer_const_constructors
+              return Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  // ignore: prefer_const_constructors
+                  CondorDeviceInfos(),
+                  // ignore: prefer_const_constructors
+                  CondorColumnButtons(),
+                ],
+              );
+            },
           ),
         ),
       ),
@@ -36,96 +47,89 @@ class CondorDeviceInfos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AppSettingsBloc, AppSettingsState>(
-      buildWhen: (previous, current) =>
-          previous.appSettingsModel.locale != current.appSettingsModel.locale,
-      builder: (context, state) {
-        return SizedBox(
-          width: 333,
-          child: Column(
+    return SizedBox(
+      width: 333,
+      child: Column(
+        children: [
+          Row(
             children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      readOnly: true,
-                      decoration: InputDecoration(
-                        labelText:
-                            condorLocalization.l10n.completeIpAddressLabel,
-                      ),
-                    ),
+              Expanded(
+                child: TextField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    labelText: condorLocalization.l10n.completeIpAddressLabel,
                   ),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.edit),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      readOnly: true,
-                      decoration: InputDecoration(
-                        labelText: condorLocalization.l10n.customNameLabel,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.edit),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              TextField(
-                readOnly: true,
-                decoration: InputDecoration(
-                  labelText: condorLocalization.l10n.serialNumberLabel,
                 ),
               ),
               const SizedBox(
-                height: 14,
+                width: 6,
               ),
-              TextField(
-                readOnly: true,
-                decoration: InputDecoration(
-                  labelText: condorLocalization.l10n.modelLabel,
-                ),
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              TextField(
-                readOnly: true,
-                decoration: InputDecoration(
-                  labelText: condorLocalization.l10n.manufacturerLabel,
-                ),
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              TextField(
-                readOnly: true,
-                decoration: InputDecoration(
-                  labelText: condorLocalization.l10n.androidVersionLabel,
-                ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.edit),
               ),
             ],
           ),
-        );
-      },
+          const SizedBox(
+            height: 14,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    labelText: condorLocalization.l10n.customNameLabel,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.edit),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: condorLocalization.l10n.serialNumberLabel,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: condorLocalization.l10n.modelLabel,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: condorLocalization.l10n.manufacturerLabel,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: condorLocalization.l10n.androidVersionLabel,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -137,79 +141,73 @@ class CondorColumnButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AppSettingsBloc, AppSettingsState>(
-      buildWhen: (previous, current) =>
-          previous.appSettingsModel.locale != current.appSettingsModel.locale,
-      builder: (context, state) {
-        return Expanded(
-          child: Wrap(
-            direction: Axis.vertical,
-            runAlignment: WrapAlignment.end,
-            crossAxisAlignment: WrapCrossAlignment.end,
-            spacing: 36,
+    return Expanded(
+      child: Wrap(
+        direction: Axis.vertical,
+        runAlignment: WrapAlignment.end,
+        crossAxisAlignment: WrapCrossAlignment.end,
+        spacing: 36,
+        children: [
+          Row(
             children: [
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 24,
-                    height: 24,
-                    child: Visibility(
-                      visible: true,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 2.5,
-                      ),
-                    ),
+              const SizedBox(
+                width: 24,
+                height: 24,
+                child: Visibility(
+                  visible: true,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2.5,
                   ),
-                  const SizedBox(
-                    width: 14,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text(condorLocalization.l10n.connectDeviceButton),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 24,
-                    height: 24,
-                    child: Visibility(
-                      visible: false,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 2.5,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 14,
-                  ),
-                  ElevatedButton(
-                    onPressed: null,
-                    child: Text(condorLocalization.l10n.disconnectDeviceButton),
-                  ),
-                ],
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text(condorLocalization.l10n.runTcpipButton),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text(condorLocalization.l10n.mirrorScreenButton),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.delete,
-                  color: Colors.red.shade600,
-                  size: 44,
                 ),
+              ),
+              const SizedBox(
+                width: 14,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(condorLocalization.l10n.connectDeviceButton),
               ),
             ],
           ),
-        );
-      },
+          Row(
+            children: [
+              const SizedBox(
+                width: 24,
+                height: 24,
+                child: Visibility(
+                  visible: false,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2.5,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 14,
+              ),
+              ElevatedButton(
+                onPressed: null,
+                child: Text(condorLocalization.l10n.disconnectDeviceButton),
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(condorLocalization.l10n.runTcpipButton),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(condorLocalization.l10n.mirrorScreenButton),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.delete,
+              color: Colors.red.shade600,
+              size: 44,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
