9f17d31d985579824f079bb0693ae88ebd89d717
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2024-09-20 16:50:12 +0200

    Update home_screen.dart
    Now the devices displayed are the one saved in the database

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wireless_debugging_devices_manager/bloc/devices_bloc/devices_bloc.dart';
+import 'package:wireless_debugging_devices_manager/models/device_model.dart';
+import 'package:wireless_debugging_devices_manager/services/condor_localization_service.dart';
 import 'package:wireless_debugging_devices_manager/widgets/condor_buttons_row.dart';
 import 'package:wireless_debugging_devices_manager/widgets/condor_device_card.dart';
 
@@ -21,32 +25,49 @@ class HomeScreen extends StatelessWidget {
               const CondorButtonsRow(),
               const SizedBox(height: 20),
               Expanded(
-                child: Scrollbar(
-                  controller: scroolController,
-                  thumbVisibility: true,
-                  child: ListView(
-                    controller: scroolController,
-                    children: const [
-                      Padding(
-                        /// this padding is used to make the scrollbar a bit more visible by giving it some space.
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 18,
-                          vertical: 9,
+                child: BlocBuilder<DevicesBloc, DevicesState>(
+                  builder: (context, state) {
+                    List<DeviceModel> devices = state.devices;
+                    if (devices.isEmpty) {
+                      return Center(
+                        child: Text(
+                          condorLocalization.l10n.noDevicesRegistered,
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                        child: Wrap(
-                          alignment: WrapAlignment.spaceEvenly,
-                          spacing: 77,
-                          runSpacing: 77,
+                      );
+                    } else {
+                      return Scrollbar(
+                        controller: scroolController,
+                        thumbVisibility: true,
+                        child: ListView(
+                          controller: scroolController,
                           children: [
-                            CondorDeviceCard(),
-                            CondorDeviceCard(),
-                            CondorDeviceCard(),
-                            CondorDeviceCard(),
+                            Padding(
+                              /// this padding is used to make the scrollbar a bit more visible by giving it some space.
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 18,
+                                vertical: 9,
+                              ),
+                              child: Wrap(
+                                alignment: WrapAlignment.spaceEvenly,
+                                spacing: 77,
+                                runSpacing: 77,
+                                children: devices.map((singleDevice) {
+                                  return CondorDeviceCard(
+                                    device: singleDevice,
+                                  );
+                                }).toList(),
+                              ),
+                            ),
                           ],
                         ),
-                      ),
-                    ],
-                  ),
+                      );
+                    }
+                  },
                 ),
               ),
             ],
